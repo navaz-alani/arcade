5ceb4887585d7f8400eabf3e17e6266f9b41a05f
Navaz Alani <nalani@uwaterloo.ca>
2020-10-08 02:07:17 -0400

    setup home page basic

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,21 @@
 import Head from "next/head";
-import styles from "@styles/Home.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>TS Arcade</title>
+        <title>Arcade</title>
       </Head>
+      <div>
+        <p>Welcome to Navaz's collection of games!</p>
+        <p>Here are the ones I have worked on so far:</p>
 
-      <div className={styles.container}>
+        <ul>
+          <li><Link href="/chess">Chess</Link></li>
+          <li><Link href="/connect4">Connect4</Link></li>
+          <li><Link href="/snake">Snake</Link></li>
+        </ul>
       </div>
     </>
   )
