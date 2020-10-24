bc19dc413b296429de633f6590c9d2dd63679f55
Navaz Alani <nalani@uwaterloo.ca>
2020-10-24 16:07:04 -0400

    added scores in view

--- src/components/pong/game/App.tsx
+++ src/components/pong/game/App.tsx
@@ -18,8 +18,8 @@ const InitialGameState =
   return {
     turn: Side.Down,
     paddles: [
-      { side: Side.Up,   pos: { x: 0, y: 0 }, ref: refs[0] },
-      { side: Side.Down, pos: { x: 0, y: 0 }, ref: refs[1] },
+      { side: Side.Up,   pos: { x: 300, y: 0 }, ref: refs[0] },
+      { side: Side.Down, pos: { x: 300, y: 0 }, ref: refs[1] },
     ],
     ball: {
       pos: { x: 0, y: 0 },
@@ -40,6 +40,7 @@ const InitialGameState =
 
 const GameReducer = (s: GameState, a: Action): GameState => {
   if (a === "reset") return InitialGameState([s.paddles[0].ref, s.paddles[1].ref]);
+  const ballRect: DOMRect = s.ball.ref.current.getBoundingClientRect();
   const contWidth: number = s.container.current.clientWidth;
   const contHeight: number = s.container.current.clientHeight;
   if (a === "move-ball") {
@@ -48,40 +49,72 @@ const GameReducer = (s: GameState, a: Action): GameState => {
       y: s.ball.pos.y + s.ball.dir.y,
     }
     const width: number = s.ball.ref.current.clientWidth;
+
+    // check for collision with the paddles
+    for (let i = 0; i < s.paddles.length; ++i) {
+      let p: DOMRect = s.paddles[i].ref.current.getBoundingClientRect();
+      let absPos: Pos = {
+        x: ballRect.x + s.ball.dir.x,
+        y: ballRect.y + s.ball.dir.y,
+      };
+      // check for collision
+      if (p.x - width <= absPos.x && absPos.x <= p.x + p.width &&
+          p.y <= absPos.y && absPos.y <= p.y - p.height) {
+        // set new velocity
+        s.ball.dir.x = -(s.ball.dir.y/20) * (p.x- ballRect.x)
+        s.ball.dir.y *= -1;
+        // set new transform
+        s.ball.pos.x = newPos.x;
+        s.ball.pos.y = (i === 0) ? p.height : contHeight - width;
+        s.ball.ref.current.style.transform =
+          `translate(${s.ball.pos.x}px, ${s.ball.pos.y}px)`;
+        s.turn = (s.turn === Side.Down) ? Side.Up : Side.Down;
+        return { ...s };
+      }
+    }
+
     // left wall
     if (newPos.x < 0) { newPos.x = 0; s.ball.dir.x *= -1; }
     // right wall
     if (newPos.x + width > contWidth) {
-      newPos.x = contWidth - width;
-      s.ball.dir.x *= -1;
+      newPos.x = contWidth - width; s.ball.dir.x *= -1;
     }
+    let point: { side: Side, rallyEnd: boolean } = { side: null, rallyEnd: false };
     // top wall
     if (newPos.y < 0) {
-      // down takes point - reset
-      newPos.y = 0;
-      s.ball.dir.y *= -1;
-      console.log("point down")
+      newPos.y = 0; s.ball.dir.y *= -1;
+      point.side = Side.Down; point.rallyEnd = true;
     }
     // bottom wall
     if (newPos.y + width > contHeight){
-      // up takes point - reset
-      newPos.y = contHeight - width;
-      s.ball.dir.y *= -1;
-      console.log("point up")
+      newPos.y = contHeight - width; s.ball.dir.y *= -1;
+      point.side = Side.Up; point.rallyEnd = true;
     }
 
     s.ball.pos.x = newPos.x; s.ball.pos.y = newPos.y;
     s.ball.ref.current.style.transform =
       `translate(${s.ball.pos.x}px, ${s.ball.pos.y}px)`;
+    if (point.rallyEnd) {
+      ++s.score[point.side];
+      s.ball.pos.x = 300; s.ball.pos.y = 300;
+      s.ball.ref.current.style.transform =
+        `translate(${s.ball.pos.x}px, ${s.ball.pos.y}px)`;
+      s.ball.dir = {
+        x: Math.floor(Math.random()*20 - 10),
+        y: Math.floor(Math.random()*20 + 10),
+      }
+      s.turn = Side.Down;
+    }
     return { ...s };
   }
 
-  const shift: number = 20 * ((a.direction === PaddleDir.Left) ? -1 : 1);
+  // move current player's paddle
+  const shift: number = 35 * ((a.direction === PaddleDir.Left) ? -1 : 1);
   let paddle: PaddleT = s.paddles[s.turn];
   const width: number = paddle.ref.current.clientWidth;
-  if (paddle.pos.x + shift < 0 || paddle.pos.x + shift + width > contWidth)
-    return s;
-  paddle.pos.x += shift;
+  if (paddle.pos.x + shift < 0) paddle.pos.x = 0;
+  else if (paddle.pos.x + shift + width > contWidth) paddle.pos.x = contWidth - width;
+  else paddle.pos.x += shift;
   let pos: Pos  = s.paddles[s.turn].pos;
   s.paddles[s.turn].ref.current.style.transform = `translate(${pos.x}px, 0px)`;
 
@@ -120,11 +153,16 @@ const PongGame: React.FC = () => {
   }, [keyHandler]);
 
   return (
+    <div className={styles["app-container"]}>
+    <p>{gs.score[0]}</p>
     <div className={styles["app"]} ref={appContainer}>
       <Paddle paddle={gs.paddles[0]} />
       <Ball   ball={gs.ball} />
       <Paddle paddle={gs.paddles[1]} />
     </div>
+    <p>{gs.score[1]}</p>
+    </div>
+
   );
 }
 
